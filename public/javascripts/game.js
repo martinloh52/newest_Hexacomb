9a859befe7c4fcb234651a3715067ff8c1ff382d
DESKTOP-6D6548H\WBL <a@a.com>
2021-01-17 00:38:51 +0200

    had x and y flipped when i made the last commit. oopsies

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -6,7 +6,7 @@ for(let i = 0; i < rows.length; i++){
 
     let buttonsInRow = rows[i].children;
     for(let j = 0; j < buttonsInRow.length; j++){
-        buttonsInRow[j].id = i + "," + j;
+        buttonsInRow[j].id = j + "," + i;
 
         buttonsInRow[j].addEventListener("click", alertMyId);
     }

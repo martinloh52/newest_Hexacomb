865df15b70963d099f0a4e7618950af6a155ba0e
Martin <m.b.loh@student.tudelft.nl>
2021-07-12 23:38:28 +0200

    if this works i'll be damned

--- statTracker.js
+++ statTracker.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-const simpleGit = require('simple-git');
-const git = simpleGit();
 
 var gameStatus = {
     since: Date.now() /* since we keep it simple and in-memory, keep track of when this object was created */,
@@ -13,7 +11,19 @@ var gameStatus = {
     },
     increaseGamesCompleted: function(){
       fs.writeFileSync("plays.txt", String(parseInt(this.getGamesCompleted()) + 1));
-    }
+
+      const USER = process.env.GITHUB_USERNAME;
+      const PASS = process.env.GITHUB_USERNAME;
+      const REPO = process.env.GITHUB_REPO;
+
+      const git = require('simple-git');
+      const remote = `https://${USER}:${PASS}@${REPO}`;
+
+      await git().silent(true)
+        .add("plays.txt")
+        .commit("heeheehoohoo, here i go committing the statistics again")
+        .push(remote, 'origin/master');
+      }
   };
   
   module.exports = gameStatus;
